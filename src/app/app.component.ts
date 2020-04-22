20986ce689fbd2fdaddb63a15a662e112b269fd0
Mikkel Anderson <s184230@student.dtu.dk>
2020-04-22 14:33:41 +0200

    Webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {  } from '@fortawesome/free-regular-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'software-uniguld';
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faGithub = faGithub;
+  faBars = faBars;
+
+  showSidebar = false;
+  hideMenu() {
+    this.showSidebar = !this.showSidebar;
+  }
 }
