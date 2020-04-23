1e86d18cd69595ba2c9d1ce9686c1e1021aee4f3
Mikkel Anderson <s184230@student.dtu.dk>
2020-04-23 23:11:36 +0200

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: '__APIKEY__',
+    authDomain: 'software-uniguld.firebaseapp.com',
+    databaseURL: 'https://software-uniguld.firebaseio.com',
+    projectId: 'software-uniguld',
+    storageBucket: 'software-uniguld.appspot.com',
+    messagingSenderId: '180769210786',
+    appId: '1:180769210786:web:7df9e98189e65ddea7f2fb',
+    measurementId: 'G-YFBNMXT6WC'
+  }
 };
